79643c8a4d9716b28744f16bb99c13d5b64eac2d
sebbbas1507 <juansebastianms1507@gmail.com>
2026-05-13 13:00:07 -0500

    IA local con contexto, personalidad, prompts

--- main.py
+++ main.py
@@ -1,42 +1,43 @@
 import ollama
 
-# Historial de conversación
-mensajes = []
+# Historial con mensaje de sistema
+mensajes = [
+    {
+        'role': 'system',
+        'content': (
+            'Eres una IA de asistencia computacional como cortana. '
+            'Responde de forma clara, educativa y corta.'
+            'Habla como un robot.'
+        )
+    }
+]
 
 print("=== IA LOCAL ===")
 print("Escribe 'salir' para terminar.\n")
 
 while True:
 
-    # Pedir mensaje al usuario
     texto = input("Tú: ")
 
-    # Salir del programa
     if texto.lower() == "salir":
         print("Cerrando chat...")
         break
 
-    # Agregar mensaje del usuario
     mensajes.append({
         'role': 'user',
         'content': texto
     })
 
-    # Enviar conversación completa a la IA
     respuesta = ollama.chat(
         model='llama3.2',
         messages=mensajes
     )
 
-    # Extraer respuesta
     contenido = respuesta['message']['content']
 
-    # Mostrar respuesta
     print(f"\nIA: {contenido}\n")
 
-    # Guardar respuesta en historial
     mensajes.append({
         'role': 'assistant',
         'content': contenido
     })
-
