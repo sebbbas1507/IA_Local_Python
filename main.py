39f45c948bad32643dce6cbf19cd3046e290a05d
sebbbas1507 <juansebastianms1507@gmail.com>
2026-05-13 14:06:41 -0500

    IA local memoria persistente, guardar mensajes en archivo json

--- main.py
+++ main.py
@@ -1,24 +1,44 @@
 import ollama
+import json
+import os           #Interactia con el sistema operativo
 
-mensajes = [
-    {
-        'role': 'system',
-        'content': (
-            'Eres una IA experta en Python. '
-            'Explica de forma clara.'
-        )
-    }
-]
+#Memoria persistente en un archivo JSON
+ARCHIVO_MEMORIA = "memoria.json"
 
-print("=== IA LOCAL STREAMING ===")
+# =========================
+# CARGAR MEMORIA
+# =========================
+
+if os.path.exists(ARCHIVO_MEMORIA):     #Verifica si el archivo de memoria existe
+
+    with open(ARCHIVO_MEMORIA, "r", encoding="utf-8") as archivo:
+        mensajes = json.load(archivo) #Convierte json a python
+
+else:
+
+    mensajes = [
+        {
+            'role': 'system',
+            'content': (
+                'Eres una IA experta en Python. '
+                'Responde claro y corto.'
+            )
+        }
+    ]
+
+print("=== IA LOCAL CON MEMORIA ===")
 print("Escribe 'salir' para terminar.\n")
+
+# =========================
+# CHAT PRINCIPAL
+# =========================
 
 while True:
 
     texto = input("Tú: ")
 
     if texto.lower() == "salir":
-        print("Cerrando chat...")
+        print("Memoria guardada.")
         break
 
     mensajes.append({
@@ -36,11 +56,11 @@ while True:
 
     respuesta_completa = ""
 
-    for chunk in stream:
+    for chunk in stream:                        #Cada chunk es una parte de la respuesta que va llegando(tocken)
 
         contenido = chunk['message']['content']
 
-        print(contenido, end="", flush=True)
+        print(contenido, end="", flush=True)    #Imprime sin salto de línea y fuerza la actualización de la pantalla
 
         respuesta_completa += contenido
 
@@ -50,3 +70,16 @@ while True:
         'role': 'assistant',
         'content': respuesta_completa
     })
+
+    # =========================
+    # GUARDAR MEMORIA
+    # =========================
+
+    with open(ARCHIVO_MEMORIA, "w", encoding="utf-8") as archivo:
+
+        json.dump(      #Convierte python a json
+            mensajes,
+            archivo,
+            ensure_ascii=False, #Permite caracteres no ASCII (como acentos), caracteres en espanol
+            indent=4    #Hace el json legible con sangría
+        )
